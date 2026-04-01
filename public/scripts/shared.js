6e36141ecdcdbf59481433a8c84431522e483a1c
Anthony Cruz <antho.cr04@gmail.com>
2026-03-31 18:10:12 -0700

    changed page location to fix script

--- public/scripts/shared.js
+++ public/scripts/shared.js
@@ -8,13 +8,13 @@ window.addEventListener('DOMContentLoaded', () => {
     header.innerHTML = `
     <h1 class="multicolor-text"> Ant's Archive</h1>
     <div id = "right-overlap" >
-        <img src="../images/5ptstar.gif" alt="star gif" style="bottom: 1.7em;">
+        <img src="images/5ptstar.gif" alt="star gif" style="bottom: 1.7em;">
     </div>
     <div id="left-overlap">
-        <img src="../images/star.gif" alt="gold and silver star gif" style="bottom: -0.4em;">
+        <img src="images/star.gif" alt="gold and silver star gif" style="bottom: -0.4em;">
     </div>`;
     nav.innerHTML = `
-    <a href="../index.html">Home</a>
+    <a href="index.html">Home</a>
     <a href="guestbook.html">Guestbook</a>
     <a href="media.html">Media</a>
     <a href="gallery.html">Gallery</a>
